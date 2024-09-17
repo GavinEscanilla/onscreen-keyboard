666863d710cf8713206d0bf7b0a5f4d0faa244d2
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-09-17 02:29:06 -0400

    still researching

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
- const keyboard = {
-    keys: [
-        
+ const button = document.getElementsByClassName('key');
+ const input = document.getElementById('input');
+ const keys = [
 {
     name: '`',
     cap: '~',
+    id: 0,
+    keybind: document.getElementById('key1'),
 },
-        
+    ];
 
 
+    class keyboard{
+        constructor(){
+            this.pressed = button.addEventListener('click', () =>{
+  input.innerHTML ++ 
+            })
+        }
+    }
+ 
 
-
-
-    ]
- }
